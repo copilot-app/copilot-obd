6ed65323929429d895d8e35448c0695a61ca3258
nebcio <324930@uwr.edu.pl>
2023-04-30 14:03:55 +0200

    Add formatting options to settings.json

--- main/copilot-obd.cpp
+++ main/copilot-obd.cpp
@@ -2,48 +2,42 @@
 
 #include "TinyGPS++.h"
 
-#include "gpgga.h"
-#include "gpgll.h"
-#include "gpgsa.h"
-#include "gpgsv.h"
-#include "gprmc.h"
-#include "gptxt.h"
-#include "gpvtg.h"
-#include "nmea.h"
+#include <esp_log.h>
+#include <esp_console.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+
+#include <driver/gpio.h>
+
+#define GPIO_RX GPIO_NUM_3
+#define GPIO_TX GPIO_NUM_1
 
 TinyGPSPlus gps;
 
-void displayInfo()
-{
+void displayInfo() {
     printf("Location: ");
-    if (gps.location.isValid())
-    {
+    if (gps.location.isValid()) {
         printf("%f", gps.location.lat());
         printf(",");
         printf("%f", gps.location.lng());
-    }
-    else
-    {
+    } else
         printf("INVALID");
-    }
 
     printf("  Date/Time: ");
-    if (gps.date.isValid())
-    {
+    if (gps.date.isValid()) {
         printf("%u", gps.date.month());
         printf("/");
         printf("%u", gps.date.day());
         printf("/");
         printf("%u", gps.date.year());
-    }
-    else
-    {
+    } else {
         printf("INVALID");
     }
 
     printf(" ");
-    if (gps.time.isValid())
-    {
+    if (gps.time.isValid()) {
         if (gps.time.hour() < 10)
             printf("0");
         printf("%u", gps.time.hour());
@@ -59,18 +53,15 @@ void displayInfo()
         if (gps.time.centisecond() < 10)
             printf("0");
         printf("%u", gps.time.centisecond());
-    }
-    else
-    {
+    } else {
         printf("INVALID");
     }
 
     printf("\n");
 }
 
-void tinygpsplus_example()
-{
-    const char *gpsStream =
+void tinygpsplus_example() {
+    const char* gpsStream =
         "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
         "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
         "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
@@ -83,7 +74,6 @@ void tinygpsplus_example()
             displayInfo();
 }
 
-extern "C" void app_main(void)
-{
-    tinygpsplus_example();
+extern "C" void app_main(void) {
+    
 }
