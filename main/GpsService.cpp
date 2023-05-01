b646ff13298c74925a5274847377af3d544d9369
nebcio <324930@uwr.edu.pl>
2023-05-01 15:09:23 +0200

    gps parser -> log location

--- main/GpsService.cpp
+++ main/GpsService.cpp
@@ -1,5 +1,7 @@
 #include "GpsService.hpp"
 
+#include <string.h>
+
 #define LOG_LOCAL_LEVEL ESP_LOG_DEBUG
 #include <esp_log.h>
 
@@ -9,11 +11,16 @@ TinyGPSPlus gps;
 
 void parseGpsData(char* data, size_t size) {
     ESP_LOGI(__func__, "Parsing data");
-        ESP_LOGD(__func__, "Data: %s", data);
-    if (gps.encode(*data)){
-        if (gps.location.isValid())
-            ESP_LOGI(__func__, "Location: %f, %f", gps.location.lat(), gps.location.lng());
-    } else {
-        ESP_LOGI(__func__, "Data not parsed");
+    while (*data) {
+        if (!gps.encode(*data++))
+            continue;
+
+        if (!gps.location.isValid())
+            continue;
+
+        ESP_LOGI(__func__, "Location: %f, %f", gps.location.lat(), gps.location.lng());
+        return;
     }
+    
+    ESP_LOGI(__func__, "Data not parsed");
 }
