228d223de4f4e430619c96b240896a8a9d7c32f2
xzqvac <kawkaradoslaw@gmail.com>
2023-05-06 17:39:43 +0200

    Initialize nvs library

--- main/Bluetooth.cpp
+++ main/Bluetooth.cpp
@@ -1,6 +1,16 @@
 #include "Bluetooth.hpp"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
+void init_bluetooth() {
+    esp_err_t ret;
 
+    // Initialize NVS
+    ret = nvs_flash_init();
 
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
 
-
+}
