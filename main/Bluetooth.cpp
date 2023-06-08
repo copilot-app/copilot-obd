74b760c7007f83dd027905ccb44c5f3ab235f905
xzqvac <kawkaradoslaw@gmail.com>
2023-06-08 19:03:02 +0200

    Add handler for event read from server

--- main/Bluetooth.cpp
+++ main/Bluetooth.cpp
@@ -217,6 +217,19 @@ static void gatts_profile_OBD_event_handler(esp_gatts_cb_event_t event, esp_gatt
     //               param->update_conn_params.latency,
     //               param->update_conn_params.timeout);
     //     break;
+    case ESP_GATTS_READ_EVT: {
+        ESP_LOGI(GATTS_TAG, "GATT_READ_EVT, conn_id %d, trans_id %" PRIu32 ", handle %d\n", param->read.conn_id, param->read.trans_id, param->read.handle);
+        esp_gatt_rsp_t rsp;
+        memset(&rsp, 0, sizeof(esp_gatt_rsp_t));
+        rsp.attr_value.handle = param->read.handle;
+        rsp.attr_value.len = 3;
+        rsp.attr_value.value[0] = 'O';
+        rsp.attr_value.value[1] = 'B';
+        rsp.attr_value.value[2] = 'D';
+        esp_ble_gatts_send_response(gatts_if, param->read.conn_id, param->read.trans_id,
+                                    ESP_GATT_OK, &rsp);
+        break;
+    }
     default:
         break;
     }
