812e7100ae834de27947b53b22bdd9624bd818f5
xzqvac <kawkaradoslaw@gmail.com>
2023-06-10 18:02:10 +0200

    Add: serial port protocol callback

--- main/Bluetooth.cpp
+++ main/Bluetooth.cpp
@@ -6,6 +6,7 @@
 
 #define GATTS_TAG                   "OBD-Server"
 #define SPP_TAG                     "OBD-Client"
+#define EXAMPLE_DEVICE_NAME         "OBDII"
 #define DEVICE_NAME                 "Copilot Device"
 
 #define PROFILE_NUM                 2
@@ -102,10 +103,18 @@ static struct gatts_profile_inst gl_profile_tab[PROFILE_NUM] = {
     }
 };
 
+static struct timeval time_new, time_old;
+
+static const esp_spp_sec_t sec_mask = ESP_SPP_SEC_AUTHENTICATE;
+static const esp_spp_role_t role_master = ESP_SPP_ROLE_MASTER;
+
 esp_bd_addr_t peer_bd_addr = {0};
 static uint8_t peer_bdname_len;
 static char peer_bdname[ESP_BT_GAP_MAX_BDNAME_LEN + 1];
 static const char remote_device_name[] = "OBDII"; // to fill in
+static const esp_bt_inq_mode_t inq_mode = ESP_BT_INQ_MODE_GENERAL_INQUIRY;
+static const uint8_t inq_len = 30;
+static const uint8_t inq_num_rsps = 0;
 
 static void gatts_profile_OBD_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t* param) {
     switch (event) {
@@ -293,6 +302,150 @@ static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param
     }
 }
 
+#if (SPP_SHOW_MODE == SPP_SHOW_DATA)
+#define SPP_DATA_LEN 20
+#else
+#define SPP_DATA_LEN ESP_SPP_MAX_MTU
+#endif
+static uint8_t spp_data[SPP_DATA_LEN];
+static uint8_t *s_p_data = NULL; /* data pointer of spp_data */
+
+// static char *bda2str(uint8_t * bda, char *str, size_t size)
+// {
+//     if (bda == NULL || str == NULL || size < 18) {
+//         return NULL;
+//     }
+
+//     uint8_t *p = bda;
+//     sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
+//             p[0], p[1], p[2], p[3], p[4], p[5]);
+//     return str;
+// }
+
+static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+{
+    uint8_t i = 0;
+    char bda_str[18] = {0};
+
+    switch (event) {
+    case ESP_SPP_INIT_EVT:
+        if (param->init.status == ESP_SPP_SUCCESS) {
+            ESP_LOGI(SPP_TAG, "ESP_SPP_INIT_EVT");
+            esp_bt_dev_set_device_name(EXAMPLE_DEVICE_NAME);
+            esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+            esp_bt_gap_start_discovery(inq_mode, inq_len, inq_num_rsps);
+        } else {
+            ESP_LOGE(SPP_TAG, "ESP_SPP_INIT_EVT status:%d", param->init.status);
+        }
+        break;
+    case ESP_SPP_DISCOVERY_COMP_EVT:
+        if (param->disc_comp.status == ESP_SPP_SUCCESS) {
+            ESP_LOGI(SPP_TAG, "ESP_SPP_DISCOVERY_COMP_EVT scn_num:%d", param->disc_comp.scn_num);
+            for (i = 0; i < param->disc_comp.scn_num; i++) {
+                ESP_LOGI(SPP_TAG, "-- [%d] scn:%d service_name:%s", i, param->disc_comp.scn[i],
+                         param->disc_comp.service_name[i]);
+            }
+            /* We only connect to the first found server on the remote SPP acceptor here */
+            esp_spp_connect(sec_mask, role_master, param->disc_comp.scn[0], peer_bd_addr);
+        } else {
+            ESP_LOGE(SPP_TAG, "ESP_SPP_DISCOVERY_COMP_EVT status=%d", param->disc_comp.status);
+        }
+        break;
+    case ESP_SPP_OPEN_EVT:
+        if (param->open.status == ESP_SPP_SUCCESS) {
+            /* Start to write the first data packet */
+            esp_spp_write(param->open.handle, SPP_DATA_LEN, spp_data);
+            s_p_data = spp_data;
+            gettimeofday(&time_old, NULL);
+        } else {
+            ESP_LOGE(SPP_TAG, "ESP_SPP_OPEN_EVT status:%d", param->open.status);
+        }
+        break;
+    case ESP_SPP_CLOSE_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_CLOSE_EVT");
+        break;
+    case ESP_SPP_START_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_START_EVT");
+        break;
+    case ESP_SPP_CL_INIT_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_CL_INIT_EVT");
+        break;
+    case ESP_SPP_DATA_IND_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_DATA_IND_EVT");
+        break;
+    case ESP_SPP_WRITE_EVT:
+        if (param->write.status == ESP_SPP_SUCCESS) {
+            if (s_p_data + param->write.len == spp_data + SPP_DATA_LEN) {
+                /* Means the previous data packet be sent completely, send a new data packet */
+                s_p_data = spp_data;
+            } else {
+                /*
+                 * Means the previous data packet only be sent partially due to the lower layer congestion, resend the
+                 * remainning data.
+                 */
+                s_p_data += param->write.len;
+            }
+#if (SPP_SHOW_MODE == SPP_SHOW_DATA)
+            /*
+             * We only show the data in which the data length is less than 128 here. If you want to print the data and
+             * the data rate is high, it is strongly recommended to process them in other lower priority application task
+             * rather than in this callback directly. Since the printing takes too much time, it may stuck the Bluetooth
+             * stack and also have a effect on the throughput!
+             */
+
+            if (param->write.len < 128) {
+                esp_log_buffer_hex("", spp_data, param->write.len);
+                /* Delay a little to avoid the task watch dog */
+                // vTaskDelay(pdMS_TO_TICKS(10));  // fix
+            }
+#else
+            gettimeofday(&time_new, NULL);
+            data_num += param->write.len;
+            if (time_new.tv_sec - time_old.tv_sec >= 3) {
+                print_speed();
+            }
+#endif
+        } else {
+            /* Means the prevous data packet is not sent at all, need to send the whole data packet again. */
+            ESP_LOGE(SPP_TAG, "ESP_SPP_WRITE_EVT status:%d", param->write.status);
+        }
+
+        if (!param->write.cong) {
+            /* The lower layer is not congested, you can send the next data packet now. */
+            esp_spp_write(param->write.handle, spp_data + SPP_DATA_LEN - s_p_data, s_p_data);
+        } else {
+            /*
+             * The lower layer is congested now, don't send the next data packet until receiving the
+             * ESP_SPP_CONG_EVT with param->cong.cong == 0.
+             */
+            ;
+        }
+
+        /*
+         * If you don't want to manage this complicated process, we also provide the SPP VFS mode that hides the
+         * implementation details. However, it is less efficient and will block the caller until all data has been sent.
+         */
+        break;
+    case ESP_SPP_CONG_EVT:
+#if (SPP_SHOW_MODE == SPP_SHOW_DATA)
+        ESP_LOGI(SPP_TAG, "ESP_SPP_CONG_EVT cong:%d", param->cong.cong);
+#endif
+        if (param->cong.cong == 0) {
+            /* Send the privous (partial) data packet or the next data packet. */
+            esp_spp_write(param->write.handle, spp_data + SPP_DATA_LEN - s_p_data, s_p_data);
+        }
+        break;
+    case ESP_SPP_SRV_OPEN_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_OPEN_EVT");
+        break;
+    case ESP_SPP_UNINIT_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_UNINIT_EVT");
+        break;
+    default:
+        break;
+    }
+}
+
 static bool get_name_from_eir(uint8_t *eir, char *bdname, uint8_t *bdname_len)
 {
     uint8_t *rmt_bdname = NULL;
@@ -397,9 +550,9 @@ static void esp_bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *pa
         break;
 #endif
 
-    case ESP_BT_GAP_MODE_CHG_EVT:
-        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_MODE_CHG_EVT mode:%d", param->mode_chg.mode);
-        break;
+        case ESP_BT_GAP_MODE_CHG_EVT:
+            ESP_LOGI(SPP_TAG, "ESP_BT_GAP_MODE_CHG_EVT mode:%d", param->mode_chg.mode);
+            break;
 
         default:
             break;
@@ -463,6 +616,12 @@ void init_bluetooth() {
     ret = esp_bt_gap_register_callback(esp_bt_gap_cb);
     if (ret) {
         ESP_LOGE(SPP_TAG, "%s gap register failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    ret = esp_spp_register_callback(esp_spp_cb);
+    if (ret) {
+        ESP_LOGE(SPP_TAG, "%s spp register failed: %s\n", __func__, esp_err_to_name(ret));
         return;
     }
 
--- main/include/Bluetooth.hpp
+++ main/include/Bluetooth.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
-#include <esp_bt.h>
-#include <esp_gap_ble_api.h>
-#include <esp_gatts_api.h>
-#include <esp_bt_defs.h>
-#include <esp_bt_main.h>
-#include <esp_gatt_common_api.h>
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
 
 #include "esp_bt_main.h"
 #include "esp_gap_bt_api.h"
 #include "esp_bt_device.h"
 #include "esp_spp_api.h"
 #include "esp_gap_bt_api.h"
+
+#include "time.h"
+#include "sys/time.h"
 
 void init_bluetooth();
 static void gatts_profile_OBD_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
